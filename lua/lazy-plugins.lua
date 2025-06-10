0eb7a07a2d7fe1caca348200f4709f7418ff571a
Kieran Canter <kicanter@amazon.com>
2025-06-10 13:50:01 -0400

    Enabled neo-tree

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,37 +1,47 @@
 require("lazy").setup({
-	"NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
+    "NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
 
-	require("plugins/colors"),
-	require("plugins/gitsigns"),
-	require("plugins/which-key"),
-	require("plugins/telescope"),
-	require("plugins/lspconfig"),
-	require("plugins/conform"),
-	require("plugins/blink-cmp"),
-	require("plugins/todo-comments"),
-	require("plugins/mini"),
-	require("plugins/treesitter"),
-	require("plugins/indent_line"),
-	require("plugins/lint"),
-	require("plugins/autopairs"),
+    require("plugins/colors"),
+    require("plugins/gitsigns"),
+    require("plugins/which-key"),
+    require("plugins/telescope"),
+    require("plugins/lspconfig"),
+    require("plugins/conform"),
+    require("plugins/blink-cmp"),
+    require("plugins/todo-comments"),
+    require("plugins/mini"),
+    require("plugins/neo-tree"),
+    require("plugins/treesitter"),
+    require("plugins/indent_line"),
+    require("plugins/lint"),
+    require("plugins/autopairs"),
+    {
+        dir = "~/candela.nvim",
+        name = "candela",
+        lazy = false,
+        enabled = true,
+        config = function()
+            require("candela").setup()
+        end
+    },
 }, {
-	ui = {
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	},
+    ui = {
+        -- If you are using a Nerd Font: set icons to an empty table which will use the
+        -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+        icons = vim.g.have_nerd_font and {} or {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            require = "🌙",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 ",
+        },
+    },
 })
