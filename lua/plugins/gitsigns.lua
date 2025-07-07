60e860a3f3d472c0c4c1d3dc583b448b351b66db
Kieran Canter <kicanter@amazon.com>
2025-07-07 15:11:32 -0400

    Fixed keymap with navigating git hunks

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -22,7 +22,7 @@ return {
                 end
 
                 -- Navigation
-                map("n", "]c", function()
+                map("n", "]h", function()
                     if vim.wo.diff then
                         vim.cmd.normal({ "]h", bang = true })
                     else
@@ -30,7 +30,7 @@ return {
                     end
                 end, { desc = "Jump to next git [h]unk" })
 
-                map("n", "[c", function()
+                map("n", "[h", function()
                     if vim.wo.diff then
                         vim.cmd.normal({ "[h", bang = true })
                     else
